e0a524b22f91623fd0a9e670572d0512d6c404ef
Marcelo Santos <rocha.mer21@gmail.com>
2021-12-24 19:41:44 -0300

    removido imports nao usados

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,34 @@
-import { Button, Flex, Heading, HStack, Icon, Link } from '@chakra-ui/react'
+import { Button, Flex, Heading, HStack, Link } from '@chakra-ui/react'
 import { FaGithub } from 'react-icons/fa'
 
 export const Header = () => {
   return (
     <Flex
-      px='200px'
-      py='20px'
-      width='full'
-      bg='black'
-      alignItems='flex-end'
-      justifyContent='space-between'
-      borderBottomColor='whiteAlpha.200'
-      borderBottomWidth='1px'
+      px="200px"
+      py="20px"
+      width="full"
+      bg="black"
+      alignItems="flex-end"
+      justifyContent="space-between"
+      borderBottomColor="whiteAlpha.200"
+      borderBottomWidth="1px"
     >
-      <Flex alignItems='flex-end'>
+      <Flex alignItems="flex-end">
         <Heading
-          color='whiteAlpha.900'
-          mr='60px'
+          color="whiteAlpha.900"
+          mr="60px"
           fontSize={20}
           letterSpacing={1.5}
         >
           Logo
         </Heading>
-        <HStack color='white' spacing='40px'>
+        <HStack color="white" spacing="40px">
           <Link>Home</Link>
           <Link>Servicos</Link>
           <Link>Sobre</Link>
         </HStack>
       </Flex>
-      <Button colorScheme='pink' ariant='solid' leftIcon={<FaGithub />}>
+      <Button colorScheme="pink" ariant="solid" leftIcon={<FaGithub />}>
         Github
       </Button>
     </Flex>
